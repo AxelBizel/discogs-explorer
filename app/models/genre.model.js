01373984e23f211718fc41da2ec791cb7bfe737a
Axel Bizel <axel.bizel@gmail.com>
2023-05-03 15:13:50 +0200

    delete sequelize models

--- app/models/genre.model.js
+++
@@ -1,9 +0,0 @@
-export default (sequelize, Sequelize) => {
-  const Genre = sequelize.define("genres", {
-    name: {
-      type: Sequelize.STRING,
-      unique: true,
-    },
-  });
-  return Genre;
-};
